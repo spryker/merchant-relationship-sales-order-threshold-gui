ac166b54229b2e288b14019d4c1957aca212dec4
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-11-29 14:11:36 +0100

    PS-6157 Implement the merchant relationship threshold GUI refactoring

---
+++ src/Spryker/Zed/MerchantRelationshipSalesOrderThresholdGui/Communication/Form/DataProvider/ThresholdGroup/AbstractMerchantRelationshipThresholdDataProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\MerchantRelationshipSalesOrderThresholdGui\Communication\Form\DataProvider\ThresholdGroup;
+
+abstract class AbstractMerchantRelationshipThresholdDataProvider
+{
+    /**
+     * @var \Spryker\Zed\SalesOrderThresholdGuiExtension\Dependency\Plugin\SalesOrderThresholdFormExpanderPluginInterface[]
+     */
+    protected $formExpanderPlugins;
+
+    /**
+     * @param \Spryker\Zed\SalesOrderThresholdGuiExtension\Dependency\Plugin\SalesOrderThresholdFormExpanderPluginInterface[] $formExpanderPlugins
+     */
+    public function __construct(array $formExpanderPlugins)
+    {
+        $this->formExpanderPlugins = $formExpanderPlugins;
+    }
+}
